546f97034a89c630be047ed723c087381a9480e0
justagithubguy23 <ashtontangyie@gmail.com>
2026-07-12 20:39:32 -0500

    added back giveRoles

--- index.js
+++ index.js
@@ -85,7 +85,44 @@ function saveCounts() {
     fs.writeFileSync("counts.json", JSON.stringify(counts, null, 2));
 }
 
+async function giveRoles(member, channel) {
 
+    const total = counts[member.id] || 0;
+
+    const roles = {
+        10: "lyn lover",
+        35: "super lyn lover",
+        50: "mega lyn lover",
+        75: "ultimate lyn lover"
+    };
+
+    for (const milestone of Object.keys(roles).map(Number)) {
+
+        if (total < milestone) continue;
+
+        const role = member.guild.roles.cache.find(
+            r => r.name === roles[milestone]
+        );
+
+        if (!role) continue;
+
+        if (!member.roles.cache.has(role.id)) {
+
+            await member.roles.add(role);
+
+            console.log(
+                `${member.user.tag} earned ${role.name}`
+            );
+
+            if (channel) {
+                channel.send(
+                    `🎉 ${member} has unlocked the **${role.name}** role!`
+                ).catch(console.error);
+            }
+
+        }
+    }
+}
 
 const botData = {
     counts,
